11694ca321b615066ef8df0605d300803fd1b8f5
Maria Jose <u20212200102@usco.edu.co>
2023-11-20 07:11:35 -0500

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  urlBase: "https://proyectopw.azurewebsites.net",
+  urlBase: "https://javaapievents.azurewebsites.net",
   urlPictures: "https://cs71003200197d6ddf1.blob.core.windows.net/uploads/"
 };
